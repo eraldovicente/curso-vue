7ba19c2afe85587038fe3980fb0de141b94b99a6
eraldovicente <eraldovicente2@gmail.com>
2022-06-04 18:44:42 -0300

    LET vs VAR vs CONST

---
+++ 01-bases-js/src/bases/01-const-let.js
@@ -0,0 +1,14 @@
+
+const nombre   = 'Tony';
+const apellido = 'Stark';
+
+console.log( nombre, apellido );
+
+if ( true ) {
+    let nombre = '';
+    //const nombre = 'Peter';
+    console.log(nombre2);
+}
+
+console.log(nombre);
+//var nombre2 = 'Peter';
