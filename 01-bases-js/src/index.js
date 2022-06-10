533eec714dc59d4621d9a1ac1b6a7d6bd9611af6
eraldovicente <eraldovicente2@gmail.com>
2022-06-10 12:20:41 -0300

    AXIOS

--- 01-bases-js/src/index.js
+++ 01-bases-js/src/index.js
@@ -1,15 +1,26 @@
+import axios from 'axios'
 
 const apiKey = 'F27QYE8jyWmmEzQoclfOXVXOtc7pX1uI'
-//https://api.giphy.com/v1/gifs/random?api_key=F27QYE8jyWmmEzQoclfOXVXOtc7pX1uI#
+//`https://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`
 
-fetch(`https://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`)
-    .then( resp => resp.json() )
-    .then(({ data }) => {
+const giphyApi = axios.create({
+    baseURL: 'https://api.giphy.com/v1/gifs',
+    params: {
+        api_key: apiKey
+    }
+})
+
+giphyApi.get('/random')
+    .then( resp => {
+
+        const { data } = resp.data
         const { url } = data.images.original
-        
+
+        // console.log(resp.data.data.images.original.url)
+        // const url = resp.data.data.images.original.url
+
         const img = document.createElement('img')
         img.src = url
 
         document.body.append( img )
     })
-    
