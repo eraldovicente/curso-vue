54f5f0d0d157d369803f3df7d6c89c707b64438d
eraldovicente <eraldovicente2@gmail.com>
2022-06-10 14:56:29 -0300

    ASYNC - AWAIT - APLICADO

--- 01-bases-js/src/bases/11-axios.js
+++ 01-bases-js/src/bases/11-axios.js
@@ -10,17 +10,19 @@ const giphyApi = axios.create({
     }
 })
 
-giphyApi.get('/random')
-    .then( resp => {
+export default giphyApi
 
-        const { data } = resp.data
-        const { url } = data.images.original
+// giphyApi.get('/random')
+    // .then( resp => {
+
+    //     const { data } = resp.data
+    //     const { url } = data.images.original
 
         // console.log(resp.data.data.images.original.url)
         // const url = resp.data.data.images.original.url
 
-        const img = document.createElement('img')
-        img.src = url
+    //     const img = document.createElement('img')
+    //     img.src = url
 
-        document.body.append( img )
-    })
+    //     document.body.append( img )
+    // })
