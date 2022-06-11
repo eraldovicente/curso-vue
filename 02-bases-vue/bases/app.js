a534f872bf2a885fa682c6424017f9f90fd75445
eraldovicente <eraldovicente2@gmail.com>
2022-06-10 22:38:58 -0300

    DIRECTIVA  V-FOR

---
+++ 02-bases-vue/bases/app.js
@@ -0,0 +1,28 @@
+
+const app = Vue.createApp({
+    // template: `
+    // <h1> Hola Mundo </h1>
+    // <p> Desde app.js</p>
+    // `
+
+    data() {
+        return {
+            quote: "I'm Batman",
+            author: 'Bruce Wayne'
+        }
+    },
+    methods: {
+        changeQuote() {
+            console.log('Hola Mundo')
+            this.author = 'Eraldo Vicente'
+
+            this.capitalize()
+        },
+        capitalize() {
+            this.quote = this.quote.toUpperCase()
+        }
+    }
+
+})
+
+app.mount('#myApp')
