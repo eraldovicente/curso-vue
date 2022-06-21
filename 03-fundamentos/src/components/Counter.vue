34a997b01a370cd906c770e212e2a4e63fc3c01d
eraldovicente <eraldovicente2@gmail.com>
2022-06-20 22:13:06 -0300

    INCREMENTAR Y DECREMENTAR

--- 03-fundamentos/src/components/Counter.vue
+++ 03-fundamentos/src/components/Counter.vue
@@ -1,18 +1,30 @@
 <template>
   <h2>Counter</h2>
   <p> {{ counter }} <sup>2</sup> = {{ squaredCounter }} </p>
+
+  <div>
+    <button @click="increase">+1</button>
+    <button @click="decrease">-1</button>
+  </div>
+
 </template>
 
 <script>
 export default {
   data() {
     return {
-      counter: 10
+      counter: 1
     }
   },
   methods: {
     getSquareValue() {
       return this.counter * this.counter
+    },
+    increase() {
+      this.counter++
+    },
+    decrease() {
+      this.counter--
     }
   },
   computed: {
@@ -24,5 +36,15 @@ export default {
 </script>
 
 <style>
+
+button {
+  background-color: #64BB87;
+  border-radius: 5px;
+  color: white; 
+  cursor: pointer;
+  margin: 0 5px;
+  padding: 5px 15px;
+  transition: 0.3s ease-in-out;
+}
 
 </style>
